cf89a828cb2a40a0966120a6a9fd8d282078a2c1
deatheyes <deatheye@yeah.net>
2017-12-26 17:01:40 +0800

    update: commands' help info

--- command.go
+++ command.go
@@ -6,42 +6,42 @@ import (
 
 var GetCommand = cli.Command {
 	Name: "get",
-	Usage: "get",
+	Usage: "get [<Path>]",
 	Description: "get path value",
 	Action: GetAction,
 }
 
 var LsCommand = cli.Command {
 	Name: "ls",
-	Usage: "ls",
+	Usage: "ls [<Path>]",
 	Description: "list dir",
 	Action: LsAction,
 }
 
 var SetCommand = cli.Command {
 	Name: "set",
-	Usage: "set",
+	Usage: "set [<Path>] <Value>",
 	Description: "set path value",
 	Action: SetAction,
 }
 
 var CdCommand = cli.Command {
 	Name: "cd",
-	Usage: "cd",
+	Usage: "cd <Path>",
 	Description: "channge dir",
 	Action: CdAction,
 }
 
 var CreateCommand = cli.Command {
 	Name: "create",
-	Usage: "create",
+	Usage: "create [<Path>] <Value>",
 	Description: "create path",
 	Action: CreateAction,
 }
 
 var DelCommand = cli.Command {
 	Name: "del",
-	Usage: "del",
+	Usage: "del <Path>",
 	Description: "delete path",
 	Action: DelAction,
 }
